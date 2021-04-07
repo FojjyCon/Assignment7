867b9a5404bc4449bdaf997b83c096748c2bbf24
FojjyCon <tuh16611@temple.edu>
2021-04-07 13:49:33 -0400

    First Commit branch 9: Added duration to the Book class and refactored the entire class.

--- app/src/main/java/temple/edu/assignment7/Book.java
+++ app/src/main/java/temple/edu/assignment7/Book.java
@@ -6,17 +6,18 @@ import android.os.Parcelable;
 public class Book implements Parcelable {
 
     private String title, author, coverURL;
-    private int id;
+    private int id, duration;
 
     public Book() {
 
     };
 
-    public Book(String title, String author, String coverURL, int id) {
+    public Book(String title, String author, String coverURL, int id, int duration) {
         this.title = title;
         this.author = author;
         this.coverURL = coverURL;
         this.id = id;
+        this.duration = duration;
     }
 
     protected Book(Parcel in) {
@@ -24,6 +25,7 @@ public class Book implements Parcelable {
         author = in.readString();
         coverURL = in.readString();
         id = in.readInt();
+        duration = in.readInt();
     }
 
     public static final Creator<Book> CREATOR = new Creator<Book>() {
@@ -70,6 +72,14 @@ public class Book implements Parcelable {
         this.id = id;
     }
 
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -81,5 +91,6 @@ public class Book implements Parcelable {
         dest.writeString(author);
         dest.writeString(coverURL);
         dest.writeInt(id);
+        dest.writeInt(duration);
     }
 }
